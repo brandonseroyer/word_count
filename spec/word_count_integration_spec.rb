ffd48e4c74e6c78ad98effbacc97578357c35d6f
Brandon Seroyer <brandonseroyer@gmail.com>
2015-08-27 16:49:59 -0700

    corrected Syntax

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -6,8 +6,8 @@ set(:show_exceptions, false)
 describe('the change machine path', {:type => :feature}) do
   it('processes the user entries and returns an integer value') do
     visit('/')
-    fill_in('string', :with => ('I love Jimmy because Jimmy is my dog!')
-    fill_in('word'), :with => ('Jimmy')
+    fill_in('string', :with => 'I love Jimmy because Jimmy is my dog!')
+    fill_in('word', :with => 'Jimmy')
     click_button('Submit')
     expect(page).to have_content(2)
   end
